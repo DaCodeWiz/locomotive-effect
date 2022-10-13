8ca59e0f6b230a8a80b3ab6f860d0162df8ce59a
DaCodeWiz <jake.pearson.1694@gmail.com>
2022-10-12 21:17:36 -0700

    initial commit

---
+++ Desktop/motion one/my-app/.cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/jakepearson/Desktop/motion one/my-app/.cache/dev-404-page.js")),
+  "component---src-pages-index-jsx": preferDefault(require("/Users/jakepearson/Desktop/motion one/my-app/src/pages/index.jsx"))
+}
+
